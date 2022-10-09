96a60bb5c060ee04e5f1c9836176264449910a0b
Ghoul12342 <114170053+Ghoul12342@users.noreply.github.com>
2022-10-09 18:25:57 +0300

    Add files via upload

---
+++ xcbzscvz.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	//1
+	/*int hp;
+	cin >> hp;
+	bool r;
+	r = hp > 0;
+	cout << boolalpha << r;*/
+	//2
+	/*int c;
+	cin >> c;
+	if (c > 20) {
+		cout << "on";
+	}
+	else {
+		cout << "off";
+	}*/
+	//3
+	/*int c, t, vla;
+	cin >> t >> vla>>c;
+	if (c > t && vla <= 80) {
+		cout << "on";
+	}
+	else {
+		cout << "off";
+	}*/
+	//4
+	/*float x, y;
+	cin >> x;
+	if (x > 0) {
+		y = sqrt(x - 2);
+		cout << y;
+	}
+	else if (x == 0) {
+		y = 0;
+		cout << y;
+	}
+	else if (x < 0) {
+		y = abs(x);
+		cout << y;
+	}*/
+	//5
+	/*int a;
+	cin >> a;
+	switch (a) {
+	case 1:cout << "большой палец"; break;
+	case 2:cout << "безымянный палец"; break;
+	case 3:cout << "средний палец"; break;
+	case 4:cout << "указательный палец"; break;
+	case 5:cout << "мизинец"; break;
+	default:cout << "ошибка"; break;
+	
+	
+	}*/
+
+
+
+
+
+
+
+
+
+
+
+
+}
